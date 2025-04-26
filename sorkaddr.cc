3398e51ef57f9fb704c54b0b13c7e32a1ff49b92
linnaid <jiaguoyangyang@qq.com>
2025-04-26 21:24:17 +0800

    sockaddr

--- sorkaddr.cc
+++ sorkaddr.cc
@@ -23,3 +23,9 @@ int accept(int socket, struct socketaddr* address, socklen_t* address_len);
 //struct socketaddr* address会自动区分本地通信还是unix域间通信；
 //AF_INET是本地通信；
 //AF_UNIX是unix域间通信(也就是网络通信)；
+
+//设置套接字
+int socket(int domain, int type, int protocol);
+//domain是协议族，指定使用哪种通信协议，AF_UNIX是本地通信；AF_INET是网络通信；
+//type是通信类型，指定连接方式，UDP数据报是SOCK_DGRAM；TCP流式是SOCK_STREAM；
+//protocol是指定具体协议，通常填 0（默认值，根据前面两项自动选）
