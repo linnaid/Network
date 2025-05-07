bf1c8145908ff1d5208729f4b165029d076ae963
linnaid <jiaguoyangyang@qq.com>
2025-05-07 17:59:05 +0800

    TCPClient

---
+++ Task_one/TCPClient.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+
+int main()
+{
+    const char* Server = "127.0.0.1";
+    int sockf = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockf < 0)
+    {
+        perror("Socket_Client Creat Failed");
+        return -1;
+    }
+    struct sockaddr_in f;
+    if(inet_pton(AF_INET, Server, &f.sin_addr.s_addr) <= 0)
+    {
+        perror("Invalid Address ~_~");
+        close(sockf);
+        return -1;
+    }
+    f.sin_family = AF_INET;
+    f.sin_port = htons(8080);
+    socklen_t len = sizeof(f);
+    
+    if(connect(sockf, (struct sockaddr*)&f, len) < 0)
+    {
+        perror("Connect Failed");
+        close(sockf);
+        return -1;
+    }
+    
+    while(1)
+    {
+        char buffer[1024];
+        std::cout << "Please Enter Your Message:" << std::endl;
+        std::cin >> buffer;
+        std::string str_buffer(buffer);
+        if(str_buffer == "exit")
+        {
+            ssize_t ex = send(sockf, buffer, strlen(buffer), 0);
+            if(ex < 0)
+            {
+                perror("Exit Failed");
+                return -1;
+            }
+            std::cout << "Exit Success!" << std::endl;
+            break;
+        }
+        ssize_t dec = send(sockf, buffer, strlen(buffer), 0);
+        if(dec < 0)
+        {
+            perror("Send Failed");
+            close(sockf);
+            return -1;
+        }
+        char rece[1024];
+        ssize_t re = recv(sockf, rece, sizeof(rece)-1, 0);
+        if(re >= 0)
+        {
+            std::cout << "I Received Your Answer! " << std::endl;
+        }
+        else
+        {
+            perror("Received Failed");
+        }
+    }
+
+    close(sockf);
+    return 0;
+}
