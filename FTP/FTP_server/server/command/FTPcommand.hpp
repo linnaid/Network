5f6d870f1051f4d225ac09c5f0df85df5534e69c
linnaid <jiaguoyangyang@qq.com>
2025-06-02 22:43:45 +0800

    FTPcommand.hpp

---
+++ FTP/FTP_server/server/command/FTPcommand.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "main.hpp"
+#include "FTPconnection.hpp"
+// #include "json.hpp"
+
+class command{
+public:
+    void cmd_execute(FTPconnect& conn, const std::string cmd_line);
+    
+private:
+    static void STOR(FTPconnect& conn, const std::string& path);
+    static void RETR(FTPconnect& conn, const std::string& path);
+    static void USER(FTPconnect& conn, const std::string& name);
+    static void PASS(FTPconnect& conn, const std::string& pass);
+    static void LIST(FTPconnect& conn, const std::string& path);
+    static void QUIT(FTPconnect& conn);
+    static bool Login(FTPconnect& conn);
+};
