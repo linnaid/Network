351d17b733c52aaa2d7b5bfe48ea90d5768cb6a7
linnaid <jiaguoyangyang@qq.com>
2025-06-10 22:48:18 +0800

    main

--- FTP/FTP_server/main.cc
+++ FTP/FTP_server/main.cc
@@ -7,17 +7,17 @@ int main()
     master _master;
     _master.init();
     
-    std::vector<std::unique_ptr<worker>> workers;
-    for(int i = 0; i < num; i++){
-        workers.emplace_back(std::make_unique<worker>());
-    }
+    // std::vector<std::unique_ptr<worker>> workers;
+    // for(int i = 0; i < num; i++){
+    //     workers.emplace_back(std::make_unique<worker>());
+    // }
     _master.run();
-    for(auto& _work: workers){
-        _work->start();
-    }
+    // for(auto& _work: workers){
+    //     _work->start();
+    // }
     
-    for(auto& _work: workers){
-        _work->join();
-    }
+    // for(auto& _work: workers){
+    //     _work->join();
+    // }
     return 0;
 }
